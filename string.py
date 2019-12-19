0c55d9315c9b298b502dfe1056c10574ed89f75e
JKK_ <rhemdrhf0020@gmail.com>
2019-12-19 11:48:10 +0900

    수정

--- string.py
+++ string.py
@@ -14,10 +14,11 @@ eng_name = {
 지목된영어이름 = eng_name[지목된사람]
 
 # 예) 저는 홍길동입니다. my name is hong 문자열을 만들고 싶다
-intro = '저는 ' + 지목된사람 +' 입니다.' + 'My name is' + 지목된영어이름
+# 띄어쓰기는 문자열('') 안에 입력
+intro1 = '저는 ' + 지목된사람 +'입니다. ' + 'My name is ' + 지목된영어이름
 intro2 = '저는 {}입니다. My name is {}'.format(지목된사람,지목된영어이름)
 intro3 = f'저는 {지목된사람}입니다. My name is {지목된영어이름}'
 # format(첫번째 중괄호, 두번째 중괄호)
-print(intro)
+print(intro1)
 print(intro2)
 print(intro3)
